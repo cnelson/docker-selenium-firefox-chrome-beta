0d9336857e20bbafbe14ffced037b321727fd674
Chris Nelson <Chris_Nelson@playstation.sony.com>
2015-03-11 17:08:12 +0000

    do host file injection

--- scripts/entry_point.sh
+++ scripts/entry_point.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# handle host file injection
+if [ ! -z "$HOSTS" ]; then
+   echo "$HOSTS" >> /etc/hosts
+fi
+
 export DISPLAY=:99
 
 export PATH="/opt/firefox:$PATH"
 
 Xvfb :99 -shmem -screen 0 1366x768x16 &
+
+# wait for the X server to come up
+sleep 5;
+
 x11vnc -passwd secret -display :99 -N -forever &
 
 java -jar /opt/selenium/selenium-server-standalone-2.44.0.jar "$@"
